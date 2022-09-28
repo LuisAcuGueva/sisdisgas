9cf85870c024d82467f83f3ca5b1f2b592a61410
Luis Acuña Guevara <acuna.guevara.luis.edgardo@gmail.com>
2022-09-28 17:34:20 -0500

    fix repartidor cursor pointer

--- resources/views/app/inicio/admin.blade.php
+++ resources/views/app/inicio/admin.blade.php
@@ -1,3 +1,28 @@
+<style>
+	.empleado{
+		cursor: pointer;
+		margin: 5px; 
+		width: 120px; 
+		height: 110px; 
+		text-align: center; 
+		border-style: solid; 
+		border-color: #2a3f54; 
+		border-radius: 10px;
+	}
+	.empleado-label{
+		cursor: pointer;
+		vertical-align: middle;
+		font-size: 12px; 
+		color: #2a3f54;
+	}
+	#empleados{
+		margin: 10px 0px; 
+		border-style: groove;
+		width: 100%; 
+		display: -webkit-inline-box; 
+		overflow-x: scroll; 
+	}
+</style>
 <div class="row">
 	<div class="col-lg-6 col-md-6 col-sm-6 col-xs-6">
 		<div class="x_panel">
@@ -42,21 +67,18 @@
 					{!! Form::hidden('page', 1, array('id' => 'page')) !!}
 					{!! Form::hidden('accion', 'listar', array('id' => 'accion')) !!}
 					@if(!empty($empleados))
-					<div id="empleados" style=" margin: 10px 0px; display: -webkit-inline-box; width: 100%; overflow-x: scroll; border-style: groove;">
-						@foreach($empleados  as $key => $value)
-							<div class="empleado" id="{{ $value->id}}" style="margin: 5px; width: 120px; height: 110px; text-align: center; border-style: solid; border-color: #2a3f54; border-radius: 10px;" >
-								<img src="assets/images/empleado.png" style="width: 50px; height: 50px">
-								<?php
-									$nombre_completo = $value->nombres.' '.$value->apellido_pat.' '.$value->apellido_mat;
-								?>
-								<label style="font-size: 11px;  color: #2a3f54;">{{ $nombre_completo }}</label>
-							</div>
-						@endforeach
-						{!! Form::hidden('trabajador_id',null,array('id'=>'trabajador_id')) !!}
-						{!! Form::hidden('empleado_nombre',null,array('id'=>'empleado_nombre')) !!}
-					</div>
+						<div id="empleados">
+							@foreach($empleados  as $key => $value)
+								<div class="empleado" id="{{ $value->id}}" >
+									<img src="assets/images/empleado.png" style="width: 60px; height: 60px">
+									<label class="empleado-label">{{ $value->nombres.' '.$value->apellido_pat.' '.$value->apellido_mat }}</label>
+								</div>
+							@endforeach
+							{!! Form::hidden('trabajador_id',null,array('id'=>'trabajador_id')) !!}
+							{!! Form::hidden('empleado_nombre',null,array('id'=>'empleado_nombre')) !!}
+						</div>
 					@else
-					<h4 class="page-venta" style ="margin: 10px 0px;  font-weight: 600; text-align: center; color: red;"> NO HAY REPARTIDORES EN TURNO</h4>
+						<h4 class="page-venta" style ="margin: 10px 0px;  font-weight: 600; text-align: center; color: red;"> NO HAY REPARTIDORES EN TURNO</h4>
 					@endif
 				</div>
 				<div class="form-group" style="display:none;">
@@ -64,7 +86,6 @@
 					{!! Form::selectRange('filas', 1, 30, 15, array('class' => 'form-control input-sm', 'onchange' => 'buscar(\''.$entidad_turnos.'\')')) !!}
 				</div>
 				{!! Form::close() !!}
-
 				<div id="listado{{ $entidad_turnos }}"></div>
 			</div>
 
