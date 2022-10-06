83d2df697da3127d27c9549cfa21b3f1ce8276e1
Luis Acuña Guevara <acuna.guevara.luis.edgardo@gmail.com>
2022-10-06 17:46:52 -0500

    fix list mov almacen

--- resources/views/app/movalmacen/list.blade.php
+++ resources/views/app/movalmacen/list.blade.php
@@ -5,7 +5,6 @@ use Illuminate\Support\Facades\DB;
 use App\Menuoption;
 use App\OperacionMenu;
 ?>
-
 @if(count($lista) == 0)
 <h3 class="text-warning">No se encontraron resultados.</h3>
 @else
@@ -20,45 +19,24 @@ use App\OperacionMenu;
 		</tr>
 	</thead>
 	<tbody>
-		<?php
-		$contador = $inicio + 1;
-		?>
 		@foreach ($lista as $key => $value)
-		@if($value->estado == 1)
-			<tr style ="background-color: #ffffff !important">
+			<tr style ="background-color: {{ $value->estado == 1 ? '#ffffff' : '#ffc8cb' }} !important">
 			<td align="center">{!! Form::button('', array('onclick' => 'modal (\''.URL::route($ruta["detalle"], array($value->id, 'listar'=>'SI')).'\', \''.$tituloDetalle.'\', this);', 'class' => 'btn btn-sm btn-primary glyphicon glyphicon-eye-open')) !!}</td>
-			@if(date("Y-m-d",strtotime($value->fecha)) == $hoy )
+			@if($value->estado == 1 && date("Y-m-d",strtotime($value->fecha)) == $hoy )
 				<td align="center">{!! Form::button('', array('onclick' => 'modal (\''.URL::route($ruta["delete"], array($value->id, 'listar'=>'SI')).'\', \''.$tituloEliminar.'\', this);', 'class' => 'btn btn-sm btn-danger glyphicon glyphicon-remove')) !!}</td>
 			@else
 				<td align="center">{!! Form::button('', array('onclick' => 'modal (\''.URL::route($ruta["delete"], array($value->id, 'listar'=>'SI')).'\', \''.$tituloEliminar.'\', this);', 'disabled', 'class' => 'btn btn-sm btn-secondary glyphicon glyphicon-remove')) !!}</td>
 			@endif
-		@elseif($value->estado == 0)
-			<tr style ="background-color: #ffc8cb !important">
-			<td align="center">{!! Form::button('', array('onclick' => 'modal (\''.URL::route($ruta["detalle"], array($value->id, 'listar'=>'SI')).'\', \''.$tituloDetalle.'\', this);', 'class' => 'btn btn-sm btn-primary glyphicon glyphicon-eye-open')) !!}</td>
-			<td align="center">{!! Form::button('', array('onclick' => 'modal (\''.URL::route($ruta["delete"], array($value->id, 'listar'=>'SI')).'\', \''.$tituloEliminar.'\', this);', 'disabled', 'class' => 'btn btn-sm btn-secondary glyphicon glyphicon-remove')) !!}</td>
-		@endif
-			<td align="center">{{ $fechaformato = date("d/m/Y h:i:s a",strtotime($value->fecha))}}</td>	
-			<!--td>{{ $value->trabajador->nombres .' '. $value->trabajador->apellido_pat .' '. $value->trabajador->apellido_mat}}</td-->
-			@if( $value->concepto_id == 11)
-				<td align="center" style ="background-color: #acffaab0 !important">{{ $value->concepto->concepto }}</td>
-			@else
-				<td align="center" style ="background-color: #ffb6b6cc !important">{{ $value->concepto->concepto }}</td>
-			@endif
+			<td align="center">{{ date("d/m/Y h:i:s a",strtotime($value->fecha))}}</td>	
+			<td align="center" style ="background-color: {{ $value->concepto_id == 11 ? '#acffaab0' : '#ffb6b6cc' }} !important">{{ $value->concepto->concepto }}</td>
 			<td>{{ $value->tipodocumento->abreviatura . '' .$value->num_compra }}</td>
 			@if($value->estado == 1)
-				@if (!is_null($value->comentario))
-					<td> {{ $value->comentario }} </td>
-				@else
-					<td align="center"> - </td>
-				@endif
-			@elseif($value->estado == 0)
+				<td> {{ $value->comentario ? $value->comentario : '-' }} </td>
+			@else
 				<td> {{ $value->comentario }} | Anulado por: {{ $value->comentario_anulado }} </td>
 			@endif
 			<td align="center">{{ $value->total }}</td>
 		</tr>
-		<?php
-		$contador = $contador + 1;
-		?>
 		@endforeach
 	</tbody>
 </table>
